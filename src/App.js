af7a314cebfa0385a6e593c552e2890d8dd58822
mamarilla2022 <mamarilla@netnix.net>
2022-10-24 23:30:43 -0300

    proyecto

--- src/App.js
+++ src/App.js
@@ -2,20 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  let variable = 2;
+  let label =""
+
+/*   if (variable==1){
+    label="Learn React";
+  }else{
+    label="aprender react"
+  } */
+
+
+
+  //forma sugar sintax de hacer lo mismo:
+let label2= variable ==1 ? "learn react" : variable == 2 ? "lo que sea" : "otra cosa";
+  
+//jsx ejemplos
+let aHrefStyle ={
+  color: "black"
+}
+
+
+return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p onClick={() => alert("hola mundo")}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
+         /* style={aHrefStyle} */
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {label2}
         </a>
       </header>
     </div>
